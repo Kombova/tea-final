fe7a58d417e4da550bf5297f4cd8a12f5161f2b5
John Doe <johndoe@example.com>
2023-06-20 19:11:30 +0300

    modified:   src/components/product/ControleCard.jsx 	modified:   src/components/product/ProductCard.jsx

--- src/components/product/ControleCard.jsx
+++ src/components/product/ControleCard.jsx
@@ -2,15 +2,26 @@
 import { useEffect, useState } from "react";
 const ControleCard = ({step,setPrice,initialPrice}) =>{
     let startCount = !step ? 1 : step[1];
-    const[count,setCount]=useState(startCount);
+    const[count,setCount]=useState(+startCount);
+    useEffect(()=>{
+        if(step){
+            let price = initialPrice / 1000;
+            setPrice(()=>price * count)
+        }
+        
+    })
 
     function clickPlus(){
         setCount((count)=>count + 1)
-        setPrice((price)=>initialPrice * count)
+        setPrice(()=>initialPrice * (count+1))
     }
     function clickMinus(){
         setCount((count)=> count > 1 ? count - 1 : 1)
-        setPrice((price)=>initialPrice * count)
+        setPrice(()=> count === 1 ? initialPrice : initialPrice * (count-1))
+    }
+    function clickSelect(e){
+        let newCount = +e.target.value;
+        setCount(newCount)
     }
     return(
         <div className="w-full h-12 mt-5  bg-black rounded-[10px]">
@@ -19,7 +30,16 @@ const ControleCard = ({step,setPrice,initialPrice}) =>{
                 <div className="bg-white w-[130px] h-5/6 rounded-[10px] text-[20px] font-medium flex items-center justify-center gap-2 text-center ">{+count}<span>шт</span> </div>
                 <button className="text-[40px] text-white text-center grow " onClick={()=>clickPlus()}>+</button>
             </div>}
-            {step && <div><button>-</button><span>{+count}</span><button>+</button></div>}
+
+            {step &&  <div className="flex h-full justify-evenly items-center text-white text-[16px] font-medium">
+                {step.map((step)=>{
+                        if(count === +step){
+                            return <button key={step} className="bg-white text-black w-[85px] h-5/6 rounded-[10px]">{step} гр</button>
+                        }else return <button key={step} className=" w-[85px]" onClick={(e)=> clickSelect(e)} value={step}>{step} гр</button>   
+                    })
+                }
+                </div>     
+            }
         </div>
     )
 }
--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -2,10 +2,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import ControleCard from "./ControleCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProductCard = ({product}) =>{
    const[price,setPrice]=useState(+product.fields.price)
+   
     return(  
         <li  className="w-[290px] h-[462px] rounded-[15px] shadow-2xl overflow-none" style={{ boxShadow: '2px 2px 7px rgba(34, 31, 31, 0.4)' }}>
             <div className="w-full h-48 relative rounded-t-[15px] object-cover">
@@ -13,10 +14,10 @@ const ProductCard = ({product}) =>{
                     <Image className=" rounded-t-[15px]" src={`http:${product.fields.mainImg.fields.file.url}`} fill={true} alt={product.fields.title}/>
                 </Link>
             </div>
-            <div className="px-[10px]">
+            <div className="px-[10px] mt-3">
                 <h3 className=" text-center text-[20px] font-medium">{product.fields.title}</h3>
                 <h4 className="text-center">{product.fields.category.fields.type}</h4> 
-                <p className="w-full mt-5 flex justify-between font-normal color-[#001011]">Ціна :<span className="text-[20px] font-semibold color-[#001011] ">{price}</span></p>
+                <p className="w-full mt-5 flex justify-between font-normal color-[#001011]">Ціна :<span className="text-[20px] font-semibold color-[#001011] ">{price} грн</span></p>
                 
                 <ControleCard step={product.fields.category.fields.step} setPrice={setPrice} initialPrice={+product.fields.price}/>
                 <button className=" w-full h-12 mt-5 flex justify-center gap-3 items-center text-[19px] text-white rounded-[10px] font-semibold bg-[#0E8388]"><span>В кошик</span><Image src='little-cart.svg' width={30} height={30} alt='Shopping cart' /></button>
