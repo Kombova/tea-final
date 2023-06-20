12c80186b0a3727d16e9ff5fea5341b9939fce78
John Doe <johndoe@example.com>
2023-06-20 17:34:40 +0300

    modified:   src/components/product/ControleCard.jsx 	modified:   src/components/product/ProductCard.jsx

--- src/components/product/ControleCard.jsx
+++ src/components/product/ControleCard.jsx
@@ -1,14 +1,23 @@
 'use client'
-const ControleCard = ({step,setPrice}) =>{
-    console.log(step)
-    let count = !step ? 1 : step[1];
-    
+import { useEffect, useState } from "react";
+const ControleCard = ({step,setPrice,initialPrice}) =>{
+    let startCount = !step ? 1 : step[1];
+    const[count,setCount]=useState(startCount);
+
+    function clickPlus(){
+        setCount((count)=>count + 1)
+        setPrice((price)=>initialPrice * count)
+    }
+    function clickMinus(){
+        setCount((count)=> count > 1 ? count - 1 : 1)
+        setPrice((price)=>initialPrice * count)
+    }
     return(
         <div className="w-full h-12 mt-5  bg-black rounded-[10px]">
             {!step && <div className="flex h-full justify-between items-center">
-                <button className="text-[40px] text-white text-center grow ">-</button>
+                <button className="text-[40px] text-white text-center grow " onClick={()=>clickMinus()}>-</button>
                 <div className="bg-white w-[130px] h-5/6 rounded-[10px] text-[20px] font-medium flex items-center justify-center gap-2 text-center ">{+count}<span>шт</span> </div>
-                <button className="text-[40px] text-white text-center grow ">+</button>
+                <button className="text-[40px] text-white text-center grow " onClick={()=>clickPlus()}>+</button>
             </div>}
             {step && <div><button>-</button><span>{+count}</span><button>+</button></div>}
         </div>
--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -5,7 +5,7 @@ import ControleCard from "./ControleCard";
 import { useState } from "react";
 
 const ProductCard = ({product}) =>{
-   const[price,setPrice]=useState(product.fields.price)
+   const[price,setPrice]=useState(+product.fields.price)
     return(  
         <li  className="w-[290px] h-[462px] rounded-[15px] shadow-2xl overflow-none" style={{ boxShadow: '2px 2px 7px rgba(34, 31, 31, 0.4)' }}>
             <div className="w-full h-48 relative rounded-t-[15px] object-cover">
@@ -18,7 +18,7 @@ const ProductCard = ({product}) =>{
                 <h4 className="text-center">{product.fields.category.fields.type}</h4> 
                 <p className="w-full mt-5 flex justify-between font-normal color-[#001011]">Ціна :<span className="text-[20px] font-semibold color-[#001011] ">{price}</span></p>
                 
-                <ControleCard step={product.fields.category.fields.step} setPrice={setPrice}/>
+                <ControleCard step={product.fields.category.fields.step} setPrice={setPrice} initialPrice={+product.fields.price}/>
                 <button className=" w-full h-12 mt-5 flex justify-center gap-3 items-center text-[19px] text-white rounded-[10px] font-semibold bg-[#0E8388]"><span>В кошик</span><Image src='little-cart.svg' width={30} height={30} alt='Shopping cart' /></button>
             </div>
             
