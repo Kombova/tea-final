ad0a2849b6ef79a55c7b2720b5fcd1bfa6bdf95c
John Doe <johndoe@example.com>
2023-06-26 14:58:32 +0300

    modified:   package.json 	modified:   src/app/layout.js 	modified:   src/components/product/ControleCard.jsx 	modified:   src/components/product/ProductCard.jsx 	modified:   src/components/shoppingCart/ShoppingCart.jsx 	modified:   src/stores/cart-store.js 	modified:   yarn.lock 	public/cancel-item.svg

---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { createContext, useState } from "react";
+
+export const GlobalStateContext=createContext();
+
+
+const GlobalStateProvider = ({children}) =>{
+    const[globalState,setGlobalState]=useState({shoppingCartArr: [] });
+    let useGlobalState={
+        globalState,
+        setGlobalState
+    }
+    return <GlobalStateContext.Provider value={useGlobalState}>{children}</GlobalStateContext.Provider>
+}
+
+export default GlobalStateProvider;
